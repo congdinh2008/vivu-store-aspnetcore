1f5bc6aff36ffa82f24d1d0f485a6cec1bafa71e
Cong Dinh <congdinh2012@hotmail.com>
2025-03-11 14:06:53 +0700

    Step 19: Add NUnit testing project with initial test setup

---
+++ ViVuStore.NUnitTesting/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace ViVuStore.NUnitTesting;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

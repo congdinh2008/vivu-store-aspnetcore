a295bd73528165201d1b2a5adf59d385a7975f41
Cong Dinh <congdinh2012@hotmail.com>
2025-03-14 12:56:56 +0700

    Step 28: Add AutoMapper mappings for Category, Supplier, and Product models

--- ViVuStore.API/Configuration/MappingProfile.cs
+++ ViVuStore.API/Configuration/MappingProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using ViVuStore.Business.Handlers;
 using ViVuStore.Business.ViewModels;
 using ViVuStore.Models.Common;
 
@@ -12,5 +13,21 @@ public class MappingProfile : Profile
             .ForMember(x => x.CreatedBy, opt => opt.MapFrom(e => e.CreatedBy != null ? e.CreatedBy.DisplayName: string.Empty))
             .ForMember(x => x.UpdatedBy, opt => opt.MapFrom(e => e.UpdatedBy != null ? e.UpdatedBy.DisplayName: string.Empty))
             .ForMember(x => x.DeletedBy, opt => opt.MapFrom(e => e.DeletedBy != null ? e.DeletedBy.DisplayName: string.Empty));
+
+        CreateMap<CategoryCreateUpdateCommand, Category>().ReverseMap();
+
+        CreateMap<SupplierViewModel, Supplier>().ReverseMap()
+            .ForMember(x => x.CreatedBy, opt => opt.MapFrom(e => e.CreatedBy != null ? e.CreatedBy.DisplayName: string.Empty))
+            .ForMember(x => x.UpdatedBy, opt => opt.MapFrom(e => e.UpdatedBy != null ? e.UpdatedBy.DisplayName: string.Empty))
+            .ForMember(x => x.DeletedBy, opt => opt.MapFrom(e => e.DeletedBy != null ? e.DeletedBy.DisplayName: string.Empty));
+
+        CreateMap<SupplierCreateUpdateCommand, Supplier>().ReverseMap();
+
+        CreateMap<ProductViewModel, Product>().ReverseMap()
+            .ForMember(x => x.CreatedBy, opt => opt.MapFrom(e => e.CreatedBy != null ? e.CreatedBy.DisplayName: string.Empty))
+            .ForMember(x => x.UpdatedBy, opt => opt.MapFrom(e => e.UpdatedBy != null ? e.UpdatedBy.DisplayName: string.Empty))
+            .ForMember(x => x.DeletedBy, opt => opt.MapFrom(e => e.DeletedBy != null ? e.DeletedBy.DisplayName: string.Empty));
+
+        CreateMap<ProductCreateUpdateCommand, Product>().ReverseMap();
     }
 }
